16f474bf60c3a76a48b035ad9888f342cdb79440
lunacandido <lunacandido@users.noreply.github.com>
2020-11-02 10:14:19 -0300

    python4.py

---
+++ primeiros passos em python/python4.py
@@ -0,0 +1,8 @@
+### condicional elif se usa
+### quando só iremos utilizar
+### se o if der falso
+variavel1 = int(input("digite o número 7 ou 13"))
+if variavel1 == 7:
+    print("o numero é 7")
+elif variavel1 == 13:
+    print("o numero é 13")
