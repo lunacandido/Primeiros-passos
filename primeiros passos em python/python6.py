a9052b261f28ff033ce83b7ff332bbe0736615cb
lunacandido <lunacandido@users.noreply.github.com>
2020-11-02 10:23:00 -0300

    python6.py

---
+++ primeiros passos em python/python6.py
@@ -0,0 +1,7 @@
+### usando o for
+### lembrando que ele é pra loop finito
+variavel1 = int(input("digite um numero inteiro"))
+for i in range(0,11):
+    variavel1 = variavel1 + 2
+    print(variavel1)
+    ### sabemos que vai do 0 ao 10 meu loop
