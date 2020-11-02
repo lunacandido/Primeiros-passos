8410337acf0b1cd992989d62e2ab29fb34e58cea
lunacandido <lunacandido@users.noreply.github.com>
2020-11-02 09:51:21 -0300

    primeiro pass em python

---
+++ primeiros passos em python/python.py
@@ -0,0 +1,10 @@
+###criando variáveis e fazendo conta 
+var1 = 5
+var2 = 3
+varsoma = var1 + var2
+varmulti = var1 * var2
+varsubt = var1 - var2
+vardivi = var1 / var2
+varrest = var1 % var2
+####saída de dados
+print(varsoma , varmulti , varsubt , vardivi , varrest)
