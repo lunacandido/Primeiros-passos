646bcd2ed43ad826876a6b7e671bd694d5f1394d
lunacandido <lunacandido@users.noreply.github.com>
2020-11-02 10:01:48 -0300

    python2.py

---
+++ primeiros passos em python/python2.py
@@ -0,0 +1,8 @@
+### entrada de dados
+nome = input("qual seu nome ?")
+idade = input("qual sua idade ?")
+altura = float(input("qual sua altura ?"))
+### saída dos dados 
+print("nome: {}".format(nome))
+print("idade: {}".format(idade))
+print("altura: {}".format(altura))
